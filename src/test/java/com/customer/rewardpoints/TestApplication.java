aa91e0c0c8f48766740de4dba0ac7f4e05f86a1b
varungautam1411 <archervg@gmail.com>
2024-08-20 11:04:21 -0700

    Added test case

--- src/test/java/com/customer/rewardpoints/TestApplication.java
+++ src/test/java/com/customer/rewardpoints/TestApplication.java
@@ -1,12 +1,19 @@
 package com.customer.rewardpoints;
 
 
+import com.customer.rewardpoints.entity.CustomerData;
+import com.customer.rewardpoints.entity.CustomerTransactions;
+import com.customer.rewardpoints.model.CustomerRewards;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -28,14 +35,21 @@ public class TestApplication extends RewardsPointsApplicationTest {
 
     @Test
     public void shouldReturnJSON() throws Exception {
-        this.mockMvc.perform(get("/customerRewards/101")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"));
+        this.mockMvc.perform(get("/customerRewards/101")).andDo(print()).andExpect(status().isOk()).andExpect(content().contentType("application/json"));
 
     }
 
     @Test
     public void shouldReturnException() throws Exception {
         this.mockMvc.perform(get("/customerRewards/106")).andDo(print()).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void shouldGetValidResult() throws Exception {
+
+        ResultActions response = this.mockMvc.perform(get("/customerRewards/105"));
+
+        response.andDo(print()).andExpect(result -> System.out.println(result.getResponse().getContentAsString().contains("totalCustomerRewardPoints")));
 
 
     }
